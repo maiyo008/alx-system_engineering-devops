0815c6ac29d8f847431af14f2efe341e02a03afe
maiyo008 <tonymaiyo008@gmail.com>
2023-03-24 13:11:11 +0000

    Add comments to the puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,6 @@
+# This Puppet manifest creates a file named "school" in the /tmp directory with the content "I love Puppet",
+# file permission of 0744, owner www-data, and group www-data.
+
 file { '/tmp/school':
     ensure  => file,
     content => 'I love Puppet',
