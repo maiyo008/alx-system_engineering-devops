d20400990f8807dfe4f02ba3d2161055505f6960
maiyo008 <tonymaiyo008@gmail.com>
2023-04-24 11:10:37 +0000

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Script to gather data from REST API """
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_api <employee_id>")
+        sys.exit(1)
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+    try:
+        """Get user information"""
+        user_response = requests.get(
+            "{}/users?id={}".format(base_url, employee_id))
+        user_response.raise_for_status()
+        user_info = user_response.json()[0]
+        employee_name = user_info["name"]
+
+        """Get todo list"""
+        todo_response = requests.get(
+            "{}/todos?userID={}".format(base_url, employee_id))
+        todo_response.raise_for_status()
+        todo_list = todo_response.json()
+
+        """Extract completed tasks"""
+        completed_tasks = [task for task in todo_list if task["completed"]]
+        num_completed_tasks = len(completed_tasks)
+        total_num_tasks = len(todo_list)
+
+        print("Employee {} is done with tasks({}/{}):".format(
+            employee_name, num_completed_tasks, total_num_tasks))
+        for task in completed_tasks:
+            print(f"\t {task['title']}")
+    except requests.exceptions.HTTPError as error:
+        print("Error: {}".format(error))
