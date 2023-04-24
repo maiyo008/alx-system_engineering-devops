72ebafec92906e6f5085ff67530198bdc81e4352
maiyo008 <tonymaiyo008@gmail.com>
2023-04-24 14:13:40 +0000

    Test docstring documentation

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-"""
--This script to gather data from REST API.
-- Also it takes in an employee_id parameter,
-- returns the data for the specific employee.
-- From the data we output the employees, progress with their todolists
-- It writes the output of specific employees to a csv file
+"""Spreadsheet Column Printer
+
+This script allows the user to print to the console all columns in the
+spreadsheet. It is assumed that the first row of the spreadsheet is the
+location of the columns.
+
+This tool accepts comma separated value files (.csv) as well as excel
+(.xls, .xlsx) files.
+
+This script requires that `pandas` be installed within the Python
+environment you are running this script in.
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * get_spreadsheet_cols - returns the column headers of the file
+    * main - the main function of the script
 """
 import csv
 import requests
