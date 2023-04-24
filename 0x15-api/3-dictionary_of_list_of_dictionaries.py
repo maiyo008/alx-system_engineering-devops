b578be38949ceaa25db057e4f449130e1877bc55
maiyo008 <tonymaiyo008@gmail.com>
2023-04-24 21:02:26 +0000

    Correct output for task 10

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,37 +5,29 @@ Script is used to gather data from REST API. Once we have the data,
 we export it to a JSON file, to record all users and the tasks 
 assigned to them
 """
-import json
-import requests
+from json import dump
+from requests import get
 
 
-if __name__ == "__main__":
-    base_url = "https://jsonplaceholder.typicode.com"
-    filename = "todo_all_employees.json"
-    try:
-        """Get user information"""
-        user_response = requests.get("{}/users".format(base_url))
-        user_response.raise_for_status()
-        user_info = user_response.json()
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = get(url)
+    users = response.json()
 
-        """Getting tasks for every user"""
-        for user in user_info:
-            todo_response = requests.get(
-                "{}/todos?userId={}".format(base_url, user.get("id"))
-            )
-            todo_response.raise_for_status()
-            todo_list = todo_response.json()
-
-            """exporting to a JSON file"""
-            data = {}
-            data[user.get("id")] = []
-            for task in todo_list:
-                data[user.get("id")].append({
-                    "username": user.get("username"),
-                    "task": task.get("title"),
-                    "completed": task.get("completed")
-                })
-            with open(filename, "a") as outfile:
-                json.dump(data, outfile)
-    except requests.exceptions.HTTPError as error:
-        print("Error: {}".format(error))
+    dictionary = {}
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+        url = url + '/todos/'
+        response = get(url)
+        tasks = response.json()
+        dictionary[user_id] = []
+        for task in tasks:
+            dictionary[user_id].append({
+                                        "task": task.get('title'),
+                                        "completed": task.get('completed'),
+                                        "username": username
+                                        })
+    with open('todo_all_employees.json', 'w') as file:
+        dump(dictionary, file)
