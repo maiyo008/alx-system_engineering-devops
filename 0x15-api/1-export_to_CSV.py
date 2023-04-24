57b38b16c827d618dc18e6800bc593ca782105fa
maiyo008 <tonymaiyo008@gmail.com>
2023-04-24 14:22:31 +0000

    Test docstring documentation

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,22 +1,5 @@
 #!/usr/bin/python3
-'''Spreadsheet Column Printer
-
-This script allows the user to print to the console all columns in the
-spreadsheet. It is assumed that the first row of the spreadsheet is the
-location of the columns.
-
-This tool accepts comma separated value files (.csv) as well as excel
-(.xls, .xlsx) files.
-
-This script requires that `pandas` be installed within the Python
-environment you are running this script in.
-
-This file can also be imported as a module and contains the following
-functions:
-
-    * get_spreadsheet_cols - returns the column headers of the file
-    * main - the main function of the script
-'''
+""" a script that starts a Flask web application"""
 import csv
 import requests
 import sys
